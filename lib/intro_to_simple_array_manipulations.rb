b590aef3b84d25096fcabb856fdf6bc89d81d37b
Reid Watson <j.watsonreid@gmail.com>
2020-09-01 16:26:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,21 @@ def using_concat(arr1,arr2)
 end
 
 def using_insert(arr,element)
-  arr.insert
+  arr.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at (array,integer)
+  array.delete_at(integer)
+end
